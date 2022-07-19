3d97b9b62ff4a31a80a492b42085ec168e0d4e94
Vidhan Chandra <vidhansharma01@gmail.com>
2022-07-19 15:43:01 +0530

    Made authentication-service changes

--- server/authentication-service/src/main/java/com/ujjaval/ecommerce/authenticationservice/controller/AuthController.java
+++ server/authentication-service/src/main/java/com/ujjaval/ecommerce/authenticationservice/controller/AuthController.java
@@ -10,22 +10,12 @@ import com.ujjaval.ecommerce.authenticationservice.service.CustomUserDetailsServ
 import com.ujjaval.ecommerce.authenticationservice.util.JwtUtil;
 import com.ujjaval.ecommerce.authenticationservice.util.Md5Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
